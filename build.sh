2c09a9f55a734167572044d598c8dda43471e8fd
Jahed Ahmed <jahed.public@gmail.com>
2020-11-10 19:03:44 +0000

    feat: fetch avatar from libravatar

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env -S bash -euo pipefail
 
+echo 'Installing dependencies'
+yarn install
+
 echo 'Copying vendor scripts'
-vendor_dir='web-extension/vendor/'
+vendor_dir='web-extension/vendor'
+rm -r "${vendor_dir}"
 mkdir -p "${vendor_dir}"
-cp node_modules/openpgp/dist/openpgp.js "${vendor_dir}"
-cp node_modules/webextension-polyfill/dist/browser-polyfill.js "${vendor_dir}"
+cp node_modules/openpgp/dist/openpgp.min.js "${vendor_dir}/openpgp.js"
+cp node_modules/webextension-polyfill/dist/browser-polyfill.min.js "${vendor_dir}/browser-polyfill.js"
+cp node_modules/js-md5/build/md5.min.js "${vendor_dir}/md5.js"
 
 echo 'Building web extension'
 yarn web-ext build
--- web-extension/popup/verified.js
+++ web-extension/popup/verified.js
@@ -1,5 +1,67 @@
 let state = {};
 
+/**
+ * https://developers.cloudflare.com/1.1.1.1/dns-over-https/json-format
+ */
+const getAvatarBaseUrl = async (domain) => {
+  try {
+    const res = await fetch(
+      `https://cloudflare-dns.com/dns-query?ct=application/dns-json&name=_avatars-sec._tcp.${domain}&type=SRV`
+    );
+    const { Answer: answers } = await res.json();
+    return `https://${answers[0].name}/avatar/`;
+  } catch {
+    return "https://cdn.libravatar.org/avatar/";
+  }
+};
+
+const getAvatarDataUrl = async (email) => {
+  try {
+    const [, domain] = email.split("@");
+    const baseUrl = await getAvatarBaseUrl(domain);
+    const hash = md5.hex(email.trim().toLowerCase());
+    const remoteUrl = `${baseUrl}${hash}?d=404`;
+    const dataUrl = await getImageDataUrl(remoteUrl);
+    return dataUrl;
+  } catch (error) {
+    console.warn("failed to get avatar data url", error);
+    return "";
+  }
+};
+
+/**
+ * https://wiki.libravatar.org/api/
+ */
+const getAvatarUrl = async (email) => {
+  const storageKey = `avatars/${email}`;
+  const { [storageKey]: cachedUrl } = await browser.storage.local.get(
+    storageKey
+  );
+  if (typeof cachedUrl === "string") {
+    return cachedUrl;
+  }
+
+  const dataUrl = await getAvatarDataUrl(email);
+  browser.storage.local.set({ [storageKey]: dataUrl });
+  return dataUrl;
+};
+
+const getImageDataUrl = async (src) => {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    image.onload = () => {
+      canvas.width = image.naturalWidth;
+      canvas.height = image.naturalHeight;
+      context.drawImage(image, 0, 0);
+      resolve(canvas.toDataURL("image/jpeg", 0.5));
+    };
+    image.onerror = (error) => reject(error);
+    image.src = src;
+  });
+};
+
 const updateFingerprint = () => {
   const fingerprintEl = document.getElementById("AUTHOR_FINGERPRINT");
   if (fingerprintEl.dataset.showFingerprint === "true") {
@@ -9,7 +71,7 @@ const updateFingerprint = () => {
   }
 };
 
-const update = (nextState) => {
+const update = async (nextState) => {
   state = nextState;
   const nameEl = document.getElementById("AUTHOR_NAME");
   nameEl.textContent = state.name || "Anonymous";
@@ -21,6 +83,12 @@ const update = (nextState) => {
   commentEl.textContent = state.comment || "";
 
   updateFingerprint();
+
+  if (state.email) {
+    const avatarUrl = await getAvatarUrl(state.email);
+    const avatarEl = document.getElementById("AUTHOR_AVATAR");
+    avatarEl.src = avatarUrl;
+  }
 };
 
 const prepare = () => {
